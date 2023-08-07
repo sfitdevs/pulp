bf044abe8e94d00b8f8af7e837f53b96693c8557
Aditya Yadav <ishqaddy@gmail.com>
2023-08-07 18:49:57 +0530

    add status codes to api responses

--- controllers/pulp.js
+++ controllers/pulp.js
@@ -7,43 +7,43 @@ const ak = new shortUniqueId({ dictionary: "hex", length: 10 });
 const getPulp = async (req, res) => {
   try {
     let { key } = req.params;
-    if (!key) return res.send("key not specified");
+    if (!key) return res.status(400).send({ message: "key not specified" });
     let pulpData = await database.get(key);
     if (pulpData) {
-      res.send((({ password, accessKey, ...data }) => data)(pulpData));
+      res.status(200).send((({ password, accessKey, ...data }) => data)(pulpData));
     } else {
-      res.send({ error: "pulp not found" });
+      res.status(404).send({ message: "pulp not found" });
     }
   } catch (error) {
-    res.send({ error: "internal server error" });
+    res.status(500).send({ message: "internal server error" });
   }
 }
 
 const createPulp = async (req, res) => {
   try {
     let { content, language = "txt", password = "" } = req.body;
-    if (!content) return res.send({ error: "content can't be empty" });
+    if (!content) return res.status(400).send({ message: "content can't be empty" });
     let size = (new TextEncoder()).encode(content).byteLength;
     let details = await database.put({ content, language, password, size, key: id(), accessKey: ak(), timestamp: Date.now(), views: 0 });
-    res.send((({ password, ...data }) => data)(details));
+    res.status(200).send((({ password, ...data }) => data)(details));
   } catch (error) {
-    res.send({ error: "internal server error" });
+    res.status(500).send({ message: "internal server error" });
   }
 }
 
 const deletePulp = async (req, res) => {
   try {
     let { accessKey } = req.body;
-    if (!accessKey) return res.send("accessKey not specified");
+    if (!accessKey) return res.status(400).send({ message: "accessKey not specified" });
     let { items, count } = await database.fetch({ accessKey });
     if (count) {
       await database.delete(items[0].key);
-      res.send("pulp deleted successfully");
+      res.status(200).send({ message: "pulp deleted successfully" });
     } else {
-      res.send("pulp not found");
+      res.status(404).send({ message: "pulp not found" });
     }
   } catch (error) {
-    res.send({ error: "internal server error" });
+    res.status(500).send({ message: "internal server error" });
   }
 }
 
